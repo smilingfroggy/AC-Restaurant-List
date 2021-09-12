ec8682ce2f5d658ddb4f89ec7321ca04d3369786
smilingfroggy <smilingfroggy@hotmail.com>
2021-09-12 19:00:11 +0800

    Add readme and delete unnecessary code

--- app.js
+++ app.js
@@ -14,29 +14,24 @@ app.use(express.static('public'))
 // router setting
 // 1. index 首頁
 app.get('/', (req, res) => {
-  console.log(restaurantList.results)
   res.render('index', { restaurantIntro: restaurantList.results })
 })
 
 // 2. show 特定餐廳
 app.get('/restaurants/:storeID', (req, res) => {
-  console.log(req.params.storeID)
   const storeInfo = restaurantList.results.find((store) => {
     return store.id.toString() === req.params.storeID
   })
-  console.log(storeInfo)
   res.render('show', { restaurantIntro: restaurantList.results, storeInfo: storeInfo })
 })
 
 // 3. search result 搜尋結果
 app.get('/search', (req, res) => {
-  console.log(req.query.keyword)
   const keyword = req.query.keyword
   const storeSearched = restaurantList.results.filter((store) => {
     return store.name.toLowerCase().includes(keyword.toLowerCase()) || store.category.toLowerCase().includes(keyword.toLowerCase())
   })
-  console.log(storeSearched)
-  res.render('index', { restaurantIntro: storeSearched, keyword: keyword})
+  res.render('index', { restaurantIntro: storeSearched, keyword: keyword })
 })
 
 
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Restaurant List
+A simple web application for people to show and search restaurants with names or category
+
+### Features
+- listing restaurants with pictures, names, category, and ratings
+- searching restaurants by name or category
+- selecting restaurant to get more details
+
+### How to use?
+1. Clone this repository with terminal:
+```$ git clone https://github.com/smilingfroggy/AC2-3-Restaurant-List.git```
+
+2. Install packages:```$ npm install ```
+
+3. Run it ```$ npm run dev```
+
+4. View the website at: http://localhost:3000
+
+### Environment
+- Node.js
+- Express.js
+- Express-handlebars
