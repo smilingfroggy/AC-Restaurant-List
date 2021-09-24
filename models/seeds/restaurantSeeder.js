3e1f5c57c98aeec50e6554e6999ab7d7607d7d38
smilingfroggy <smilingfroggy@hotmail.com>
2021-09-25 02:05:21 +0800

    feat: add Mongoose, restaurants models Schema, seeder

---
+++ models/seeds/restaurantSeeder.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose')
+const RestaurantList = require('../restaurants')
+const restaurantSeeder = require('../../restaurant.json')
+mongoose.connect('mongodb://localhost/restaurant-list')
+
+const db = mongoose.connection
+
+// 確認連線狀態
+db.on('error', () => {
+  console.log('Fail to connect to mongodb!')
+})
+
+console.log(restaurantSeeder.results[1].name)
+
+db.once('open', () => {
+  console.log('mongodb connected')
+  for (let i = 0; i < restaurantSeeder.results.length; i++) {
+    RestaurantList.create({
+      name: restaurantSeeder.results[i].name,
+      name_en: restaurantSeeder.results[i].name_en,
+      category: restaurantSeeder.results[i].category,
+      image: restaurantSeeder.results[i].image,
+      location: restaurantSeeder.results[i].location,
+      phone: restaurantSeeder.results[i].phone,
+      google_map: restaurantSeeder.results[i].google_map,
+      rating: restaurantSeeder.results[i].rating,
+      description: restaurantSeeder.results[i].description
+    })
+  }
+})
