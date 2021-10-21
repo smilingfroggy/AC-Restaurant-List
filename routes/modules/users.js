8aac9aa281559db37db07aec5aa513c30951bbc7
smilingfroggy <smilingfroggy@hotmail.com>
2021-10-22 00:51:21 +0800

    feat: add login and register page view

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const Users = require('../../models/users')
 
 router.get('/login', (req, res) => {
-  res.send('Login page')
+  res.render('login')
 })
 
 router.post('/login', (req, res) => {
@@ -11,10 +11,13 @@ router.post('/login', (req, res) => {
 })
 
 router.get('/register', (req, res) => {
-  res.send('register')
+  res.render('register')
 })
 
 router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword } = req.body
+  //檢查Email是否重複註冊過
+  //檢查兩密碼是否相符
   res.send('register')
 })
 
