020177b1b2f629f86b1e58d76ab9ffd834af3a2c
smilingfroggy <smilingfroggy@hotmail.com>
2021-10-23 02:15:10 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require('express')
 const app = express()
+const session = require('express-session')
 const routes = require('./routes')
 const RestaurantList = require('./models/restaurants')
 const methodOverride = require('method-override')
 const exphbs = require('express-handlebars')
 const port = 3000
 
-
 require('./config/mongoose')
+// session options
+app.use(session({
+  secret: "MySecretKey",
+  resave: false,
+  saveUninitialized: true
+}))
 
 // template engine setting
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
