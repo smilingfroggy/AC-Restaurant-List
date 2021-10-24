efc2a9be5df11a896027905c3733c8067ae938c3
smilingfroggy <smilingfroggy@hotmail.com>
2021-10-25 02:14:55 +0800

    modify Users and restaurants seeder

--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -10,42 +10,39 @@ const userSeeder = [
   {
     email: 'user1@example.com',
     password: '12345678',
-    restaurants: [1, 2, 3]
+    restaurants: [0, 1, 2]
   },
   {
     email: 'user2@example.com',
     password: '12345678',
-    restaurants: [4, 5, 6]
+    restaurants: [3, 4, 5]
   }
 ]
 
 const db = require('../../config/mongoose')
 
 db.once('open', () => {
-  for (let i = 0; i < userSeeder.length; i++) {
-    bcrypt
-      .genSalt(10)
-      .then(salt => bcrypt.hash(userSeeder[i].password, salt))
-      .then(hash => Users.create({ email: userSeeder[i].email, password: hash }))
-      .then(user => {
-        const userId = user._id
-        return Promise.all(Array.from(
-          userSeeder[i].restaurants,  //[1, 2, 3]
-          (v, _) => RestaurantList.create({
-            name: restaurantSeeder.results[v - 1].name,
-            name_en: restaurantSeeder.results[v - 1].name_en,
-            category: restaurantSeeder.results[v - 1].category,
-            image: restaurantSeeder.results[v - 1].image,
-            location: restaurantSeeder.results[v - 1].location,
-            phone: restaurantSeeder.results[v - 1].phone,
-            google_map: restaurantSeeder.results[v - 1].google_map,
-            rating: restaurantSeeder.results[v - 1].rating,
-            description: restaurantSeeder.results[v - 1].description,
-            userId
+  Promise.all(Array.from(
+    userSeeder, userSeed => {
+      console.log(userSeed)
+      bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(userSeed.password, salt))
+        .then(hash => Users.create({ email: userSeed.email, password: hash }))
+        .then(user => {
+          const userId = user._id
+          console.log(userSeed.restaurants) //[ 4, 5, 6 ]
+          const userRestaurants = userSeed.restaurants.map( i => {
+            restaurantSeeder.results[i].userId = userId
+            return restaurantSeeder.results[i]
           })
-        ))
-      })
-      .catch(err => console.log(err))
-  }
-  console.log('Done')
+          return RestaurantList.create(userRestaurants)
+        })
+    }
+  ))
+    .then(() => {
+      console.log('Done')
+      // process.exit()
+    })
+    .catch(err => console.log(err))
 })
